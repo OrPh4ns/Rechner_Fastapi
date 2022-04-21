58cc376f1a3da86fc56c57fac38a27d26c6651e7
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-04-21 20:14:56 +0100

    Fix Request Problem

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import uvicorn
+from typing import List
+############## DB & CRUD ###############################
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from fastapi import Depends, FastAPI, Request, Response, Form
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("home.html", {"request": request, "id": id})
+
+
+
+@app.get("/rechner/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.post("/mul", response_class=HTMLResponse)
+async def login_template_frontend(request: Request):
+    msg = "aa"
+    return templates.TemplateResponse("home.html", {"request": request, "msg": msg})
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
