55e589557fbbab45ab4502aee3d5d2cf5fb474b8
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-04-22 14:20:38 +0100

    getting op value from selector

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-
 import uvicorn
 from typing import List
 ############## DB & CRUD ###############################
 
 
 from fastapi import Depends, FastAPI, Request, Response, Form
-
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
-
-
 
 templates = Jinja2Templates(directory="templates")
 
 
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("home.html", {"request": request, "id": id})
-
-
-
 @app.get("/rechner/", response_class=HTMLResponse)
-async def read_item(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.post("/mul", response_class=HTMLResponse)
-async def login_template_frontend(request: Request):
-    msg = "aa"
-    return templates.TemplateResponse("home.html", {"request": request, "msg": msg})
-
-
-
-
+@app.post("/rechnen/", response_class=HTMLResponse)
+async def login(request:Request, n1: str = Form(...), n2: str = Form(...), operator: str = Form(...)):
+    erg = int(n1) * int(n2)
+    return templates.TemplateResponse("home.html", {"request": request, "erg": erg, "operator":operator})
 
 
 if __name__ == "__main__":
